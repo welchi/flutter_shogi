058268c79dda3ccf45887a98bc1cfd36f291fa22
welchi <welchizm@gmail.com>
2021-12-08 23:38:45 +0900

    Update player.dart

---
+++ lib/domain/entity/player.freezed.dart
@@ -0,0 +1,84 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'player.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$PlayerTearOff {
+  const _$PlayerTearOff();
+
+  _Player call() {
+    return const _Player();
+  }
+}
+
+/// @nodoc
+const $Player = _$PlayerTearOff();
+
+/// @nodoc
+mixin _$Player {}
+
+/// @nodoc
+abstract class $PlayerCopyWith<$Res> {
+  factory $PlayerCopyWith(Player value, $Res Function(Player) then) =
+      _$PlayerCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class _$PlayerCopyWithImpl<$Res> implements $PlayerCopyWith<$Res> {
+  _$PlayerCopyWithImpl(this._value, this._then);
+
+  final Player _value;
+  // ignore: unused_field
+  final $Res Function(Player) _then;
+}
+
+/// @nodoc
+abstract class _$PlayerCopyWith<$Res> {
+  factory _$PlayerCopyWith(_Player value, $Res Function(_Player) then) =
+      __$PlayerCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$PlayerCopyWithImpl<$Res> extends _$PlayerCopyWithImpl<$Res>
+    implements _$PlayerCopyWith<$Res> {
+  __$PlayerCopyWithImpl(_Player _value, $Res Function(_Player) _then)
+      : super(_value, (v) => _then(v as _Player));
+
+  @override
+  _Player get _value => super._value as _Player;
+}
+
+/// @nodoc
+
+class _$_Player implements _Player {
+  const _$_Player();
+
+  @override
+  String toString() {
+    return 'Player()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _Player);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+}
+
+abstract class _Player implements Player {
+  const factory _Player() = _$_Player;
+}
