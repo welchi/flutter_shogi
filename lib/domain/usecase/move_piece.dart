8bf260cb2974ebee3dac8ccbc61af45e5f1fb31e
welchi <welchizm@gmail.com>
2021-12-09 09:54:47 +0900

    Update move_piece.dart

--- lib/domain/usecase/move_piece.dart
+++ lib/domain/usecase/move_piece.dart
@@ -1,1 +1,10 @@
-class MovePiece {}
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_shogi/domain/entity/piece.dart';
+
+class MovePiece {
+  const MovePiece(this._read);
+  final Reader _read;
+  void call({
+    required Piece piece,
+  }) {}
+}
