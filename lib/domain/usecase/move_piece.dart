2843308f8dc6e7801121d3c964556b40ff01100f
welchi <welchizm@gmail.com>
2021-12-09 10:21:21 +0900

    Add a dependency on quiver

--- lib/domain/usecase/move_piece.dart
+++ lib/domain/usecase/move_piece.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_shogi/domain/entity/direction.dart';
 import 'package:flutter_shogi/domain/entity/piece.dart';
+import 'package:quiver/iterables.dart';
 
 class MovePiece {
   const MovePiece(this._read);
@@ -11,6 +12,10 @@ class MovePiece {
   }) {
     // pieceをmoveDirectionだけ動かす？(いや違うくないか？)
     // セルを渡すか
+
+    // で、そのセルへ移動できるかまずチェック
+    // 移動途中に相手のコマがあれば、そこでストップする
+
     // 移動先に相手のコマがあれば、自分の持ち駒として取得
     // 勝敗についてチェック
     // 結果をアプトプット(状態更新)
@@ -18,5 +23,12 @@ class MovePiece {
     // コマが画面を超えて移動しないかチェック
     // こまの移動可能な線上に、相手のコマがあるかチェック(PlayerをPieceに持たせる必要がある)
     //
+  }
+
+  bool _canMove({
+    required Piece piece,
+    required Direction movingDirection,
+  }) {
+    // final zippedDirection = zip(iterables)
   }
 }
