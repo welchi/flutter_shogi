5e05748548505b5bd7b24f376b1f5db77f802cfc
welchi <welchizm@gmail.com>
2021-12-10 16:29:43 +0900

    Update select_piece.dart

--- lib/domain/command/select_piece.dart
+++ lib/domain/command/select_piece.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_shogi/domain/entity/entity.dart';
 import 'package:flutter_shogi/domain/repository/board_repository.dart';
 import 'package:flutter_shogi/domain/repository/player_repository.dart';
+import 'package:vector_math/vector_math.dart';
 
 class SelectPiece {
   SelectPiece(this._read);
@@ -46,19 +47,38 @@ class SelectPiece {
 }
 
 List<Tile> getMovableTiles(
-  PieceWithOwner piece,
+  PieceWithOwner rawPiece,
   List<PieceWithOwner> pieces,
 ) {
-  final boardMaxY = Board.rowSize-1;
-  final boardMaxX = Board.colSize-1;
-
-  final movableDirections = piece.piece.movableDirections;
-  if (piece.owner == PlayerType.human) {
-    final maxUp = min(boardMaxY,)
+  final boardSizeX = Board.colSize - 1;
+  final boardSizeY = Board.rowSize - 1;
+  final owner = rawPiece.owner;
+  final piece = rawPiece.piece;
+  final movableDirections = piece.movableDirections;
+  if (owner == PlayerType.human) {
+    // final maxUp = boardSizeY - piece.position!.y;
+    // final maxRight = boardSizeX - piece.position!.x;
+    // final maxDown = piece.position!.y;
+    // final maxLeft = piece.position!.x;
+    final directions = movableDirections.expand((movement) {
+      // final indexes = List.generate(movement.count, (index) => index).toList();
+      // for (final i in indexes) {}
+      // int x=piece.position!.x.toInt();
+      // int y=piece.position!.y.toInt();
+      // final position = piece.position!;
+      final movablePositions = <Vector2>[];
+      for (var i = 0; i < movement.count; i++) {
+        final nextPosition =
+            piece.position! + (movement.direction.scaled(i + 1));
+        if (nextPosition.x > boardSizeX || nextPosition.y > boardSizeY) {
+          break;
+        }
+      }
+      // piece.position.
+      return movablePositions;
+    }).toList();
   }
 }
-
-List<>
 
 @immutable
 class PieceWithOwner {
