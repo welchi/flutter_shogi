b856e8ce260cf27cd0af96b5c4edb5d419174695
welchi <welchizm@gmail.com>
2021-12-10 13:16:15 +0900

    Add aiPiecesProvider and humanPiecesProvider

--- lib/ui/game_view_model.dart
+++ lib/ui/game_view_model.dart
@@ -1,8 +1,34 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_shogi/domain/entity/entity.dart';
 import 'package:flutter_shogi/domain/entity/player.dart';
+import 'package:flutter_shogi/state/player_state.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'game_view_model.freezed.dart';
+
+final aiPiecesProvider = Provider((ref) {
+  final pieces = ref.watch(aiPlayerRepositoryProvider).pieces;
+  return pieces
+      .map(
+        (piece) => PieceWithOwner(
+          owner: PlayerType.ai,
+          piece: piece,
+        ),
+      )
+      .toList();
+});
+
+final humanPiecesProvider = Provider((ref) {
+  final pieces = ref.watch(humanPlayerRepositoryProvider).pieces;
+  return pieces
+      .map(
+        (piece) => PieceWithOwner(
+          owner: PlayerType.human,
+          piece: piece,
+        ),
+      )
+      .toList();
+});
 
 @freezed
 class GameViewModel with _$GameViewModel {
