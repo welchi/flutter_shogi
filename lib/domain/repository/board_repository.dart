df6e51fbd4867ff69265c8c623c0cf8de01d15a6
welchi <welchizm@gmail.com>
2021-12-10 16:58:59 +0900

    Remove board_repository.dart

--- lib/domain/repository/board_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_shogi/domain/entity/entity.dart';
-
-abstract class BoardRepository {
-  /// 駒を初期化
-  void initialize(List<Piece> pieces);
-
-  /// 指定した駒が移動可能なタイルを取得
-  List<Tile> getMovableTiles(Piece piece);
-}
