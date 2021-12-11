f890fef171bfe4389ece1382b647ef476f285880
Welchi <welchizm@gmail.com>
2021-12-11 20:44:36 +0900

    Remove unused

--- lib/domain/entity/board.dart
+++ lib/domain/entity/board.dart
@@ -1,21 +1,4 @@
-import 'package:flutter_shogi/domain/entity/piece.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'board.freezed.dart';
-
-@freezed
-class Board with _$Board {
-  const factory Board({
-    required List<Tile> tiles,
-  }) = _Board;
+class Board {
   static const colSize = 9;
   static const rowSize = 9;
-}
-
-@freezed
-class Tile with _$Tile {
-  const factory Tile({
-    // required List<Piece> pieces,
-    Piece? piece,
-  }) = _Tile;
 }
