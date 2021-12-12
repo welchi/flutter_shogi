8421638bf320c7aa278d46981ab1cc79cb404c07
Welchi <welchizm@gmail.com>
2021-12-12 21:33:50 +0900

    Update select_captured_piece.dart

--- lib/domain/command/select_captured_piece.dart
+++ lib/domain/command/select_captured_piece.dart
@@ -5,6 +5,7 @@ import 'package:flutter_shogi/domain/presenter/shogi_game_presenter.dart';
 import 'package:flutter_shogi/domain/repository/player_repository.dart';
 import 'package:flutter_shogi/presentation/game_presenter.dart';
 import 'package:flutter_shogi/state/player_state.dart';
+import 'package:vector_math/vector_math.dart';
 
 final selectCapturedPieceProvider = Provider(
   (ref) => SelectCapturedPiece(
@@ -31,8 +32,25 @@ class SelectCapturedPiece {
   }) {
     final playerPieces = playerRepository.getPieces();
     final rivalPieces = rivalPlayerRepository.getPieces();
-    final playerId = playerRepository.getId();
-    final board = getPieceMatrix([...playerPieces, ...rivalPieces]);
-    // final movablePositions =
+    final board = getPieceMatrix(
+      [...playerPieces, ...rivalPieces],
+    );
+    final movablePositions = board
+        .asMap()
+        .entries
+        .expand(
+          (rowEntry) => rowEntry.value.asMap().entries.map<Vector2>(
+                (colEntry) => Vector2(
+                  colEntry.key.toDouble(),
+                  rowEntry.key.toDouble(),
+                ),
+              ),
+        )
+        .toList();
+    // 移動可能な駒をハイライト
+    shogiGamePresenter.selectedPieceToMove(
+      piece,
+      movablePositions,
+    );
   }
 }
