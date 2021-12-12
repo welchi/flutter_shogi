060eae138100eafdbd50152bcda7405de38cfbf1
Welchi <welchizm@gmail.com>
2021-12-12 19:00:16 +0900

    Update to show captured pieces

--- lib/presentation/game_page.dart
+++ lib/presentation/game_page.dart
@@ -1,8 +1,10 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_shogi/domain/command/command.dart';
+import 'package:flutter_shogi/domain/entity/entity.dart';
 import 'package:flutter_shogi/presentation/game_view_model.dart';
 
 import 'game_presenter.dart';
@@ -19,13 +21,83 @@ class GamePage extends ConsumerWidget {
       body: Column(
         children: const [
           // 相手の持ち駒
+          SizedBox(
+            height: 50,
+            child: RivalCapturedPiecesView(),
+          ),
           Flexible(
             child: BoardView(),
           ),
           // 自分の持ち駒
+          SizedBox(
+            height: 50,
+            child: PlayerCapturedPiecesView(),
+          ),
         ],
       ),
     );
+  }
+}
+
+class PlayerCapturedPiecesView extends ConsumerWidget {
+  const PlayerCapturedPiecesView({
+    Key? key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pieces = ref.watch(playerCapturedPiecesProvider);
+    return CapturedPiecesView(
+      capturedPieces: pieces,
+    );
+  }
+}
+
+class RivalCapturedPiecesView extends ConsumerWidget {
+  const RivalCapturedPiecesView({
+    Key? key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pieces = ref.watch(rivalCapturedPiecesProvider);
+    return CapturedPiecesView(
+      capturedPieces: pieces,
+    );
+  }
+}
+
+class CapturedPiecesView extends StatelessWidget {
+  const CapturedPiecesView({
+    Key? key,
+    required this.capturedPieces,
+  }) : super(key: key);
+  final List<Piece> capturedPieces;
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      physics: const NeverScrollableScrollPhysics(),
+      crossAxisCount: 9,
+      children: capturedPieces
+          .map(
+            (piece) => Center(
+              child: TileText(
+                piece: piece,
+              ),
+            ),
+          )
+          .toList(),
+    );
+    // return Wrap(
+    //   children: capturedPieces
+    //       .map(
+    //         (piece) => Padding(
+    //           padding: const EdgeInsets.all(8),
+    //           child: TileText(
+    //             piece: piece,
+    //           ),
+    //         ),
+    //       )
+    //       .toList(),
+    // );
   }
 }
 
@@ -38,6 +110,7 @@ class BoardView extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final tiles = ref.watch(flattenTilesProvider);
     return GridView.count(
+      physics: const NeverScrollableScrollPhysics(),
       crossAxisCount: 9,
       children: tiles
           .map(
@@ -86,7 +159,7 @@ class TileView extends ConsumerWidget {
           ),
           child: Center(
             child: TileText(
-              tile: tile,
+              piece: tile.piece,
             ),
           ),
         ),
@@ -98,29 +171,29 @@ class TileView extends ConsumerWidget {
 class TileText extends ConsumerWidget {
   const TileText({
     Key? key,
-    required this.tile,
+    required this.piece,
   }) : super(key: key);
-  final HighlightableBoardTile tile;
+  final Piece? piece;
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final piece = tile.piece;
+    // final piece = tile.piece;
     // 駒を配置していないマスは何も表示しない
     if (piece == null) {
       return const SizedBox();
     }
     final rivalId = ref.watch(rivalIdProvider);
 
-    final isRival = tile.piece?.ownerId == rivalId;
+    final isRival = piece?.ownerId == rivalId;
 
     return isRival
         ? Transform.rotate(
             angle: pi,
             child: Text(
-              tile.piece?.name ?? 'Null',
+              piece?.name ?? 'Null',
             ),
           )
         : Text(
-            tile.piece?.name ?? 'Null',
+            piece?.name ?? 'Null',
           );
   }
 }
