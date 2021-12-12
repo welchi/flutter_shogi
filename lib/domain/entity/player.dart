2632f6402f325bba2cbe2e140a1ff3bc5e0d9507
Welchi <welchizm@gmail.com>
2021-12-12 17:04:04 +0900

    Update Player structure

--- lib/domain/entity/player.dart
+++ lib/domain/entity/player.dart
@@ -10,13 +10,18 @@ enum PlayerType {
 
 @freezed
 class Player with _$Player {
-  // const factory Player() = _Player;
-  const factory Player.human({
+  const factory Player({
+    required String id,
+    required PlayerType type,
     required List<Piece> pieces, // 盤面上に置いている駒
     required List<Piece> capturedPieces, // 持ち駒
-  }) = Human;
-  const factory Player.ai({
-    required List<Piece> pieces, // 盤面上に置いている駒
-    required List<Piece> capturedPieces, // 持ち駒
-  }) = AI;
+  }) = _Player;
+  // const factory Player.human({
+  //   required List<Piece> pieces, // 盤面上に置いている駒
+  //   required List<Piece> capturedPieces, // 持ち駒
+  // }) = Human;
+  // const factory Player.ai({
+  //   required List<Piece> pieces, // 盤面上に置いている駒
+  //   required List<Piece> capturedPieces, // 持ち駒
+  // }) = AI;
 }
